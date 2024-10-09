02479fcf53419cbc62c2a2a76473bbdfc29c1486
Gabriel de Melo Marcondes <gabriel-santos1313@hotmail.com>
2024-10-08 21:49:11 -0300

    :sparkles: feat: using the button component of theme settings

--- src/app/(drawer)/configurations.tsx
+++ src/app/(drawer)/configurations.tsx
@@ -1,17 +1,15 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import { colors } from '@/styles/colors';
-import { MyStorage } from '@/hooks/useAsyncStorage';
-import { useState } from 'react';
-import { useColorScheme } from 'nativewind';
+import { Text, View } from 'react-native'
+import { useColorScheme } from 'nativewind'
+
+import { MyStorage } from '@/hooks/useAsyncStorage'
+
+import { ButtonThemeConfiguration } from '@/components/buttonThemeConfiguration'
 
 export default function ConfigurationsPage() {
-  const { colorScheme, setColorScheme } = useColorScheme()
-  const [activeTheme, setActiveTheme] = useState<'light' | 'dark' | undefined>(colorScheme)
+  const { setColorScheme } = useColorScheme()
+  const storage = new MyStorage()
 
   function handleSetTheme(theme: 'light' | 'dark') {
-    setActiveTheme(theme)
-    const storage = new MyStorage()
     storage.setItem('theme', theme)
     setColorScheme(theme)
   }
@@ -22,37 +20,15 @@ export default function ConfigurationsPage() {
       <View className='gap-4 p-4'>
         <Text className='text-xl text-tint-light dark:text-tint-dark'>Tema</Text>
         <View className='gap-2'>
-          <TouchableOpacity
-            className={`flex flex-row p-4 items-center gap-4 border-4 rounded-lg ${activeTheme === 'light' ? 'border-theme bg-theme/15' : 'border-withoutHighlight-light dark:border-withoutHighlight-dark'}`}
+          <ButtonThemeConfiguration
+            theme='light'
             onPress={() => handleSetTheme('light')}
-          >
-            <Feather
-              name='sun'
-              color={activeTheme === 'light' ? colors.theme.DEFAULT : colorScheme === 'dark' ? colors.withoutHighlight.dark : colors.withoutHighlight.light}
-              size={32}
-            />
-            <Text
-              className={activeTheme === 'light' ? 'text-theme' : 'text-withoutHighlight-light dark:text-withoutHighlight-dark'}
-            >
-              Claro
-            </Text>
-          </TouchableOpacity>
+          />
 
-          <TouchableOpacity
-            className={`flex flex-row p-4 items-center gap-4 border-4 rounded-lg ${activeTheme === 'dark' ? 'border-theme bg-theme/15' : 'border-withoutHighlight-light dark:border-withoutHighlight-dark'}`}
+          <ButtonThemeConfiguration
+            theme='dark'
             onPress={() => handleSetTheme('dark')}
-          >
-            <Feather
-              name='moon'
-              color={activeTheme === 'dark' ? colors.theme.DEFAULT : colorScheme === 'dark' ? colors.withoutHighlight.dark : colors.withoutHighlight.light}
-              size={32}
-            />
-            <Text
-              className={activeTheme === 'dark' ? 'text-theme' : 'text-withoutHighlight-light dark:text-withoutHighlight-dark'}
-            >
-              Escuro
-            </Text>
-          </TouchableOpacity>
+          />
         </View>
       </View>
     </View>
