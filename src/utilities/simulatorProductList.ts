f45ed7b96336904ffae46c20cedd814861c5b571
Gabriel de Melo Marcondes <gabriel-santos1313@hotmail.com>
2024-10-08 21:38:56 -0300

    :sparkles: feat: products with promotional prices; function to create the list in SectionList format

--- src/utilities/simulatorProductList.ts
+++ src/utilities/simulatorProductList.ts
@@ -1,220 +1,148 @@
+export type TProductList = {
+  title: string
+  data: IData[]
+}[]
+
 export interface IData {
   id: string
   name: string
   amount: number
   quantity: number
+  promotionalAmount: number
+  promotionalQuantity: number
+  section: string
   isCollected: boolean
+}
+
+export function generateSectionListItems() {
+  const sectionList = simulatorProductList.reduce((acc, product) => {
+    // Verifica se já existe uma seção com o mesmo nome
+    const sectionIndex = acc.findIndex(section => section.title === product.section)
+
+    // Se a seção não existir, cria uma nova
+    if (sectionIndex === -1) {
+      acc.push({
+        title: product.section,
+        data: [product],
+      })
+    } else {
+      // Se a seção já existir, adiciona o produto à seção correspondente
+      acc[sectionIndex].data.push(product)
+    }
+
+    return acc
+  }, [] as TProductList)
+
+  // Ordena as seções por título
+  sectionList.sort((sa, sb) => sa.title.localeCompare(sb.title))
+
+  // Ordena os produtos dentro de cada seção
+  sectionList.forEach(section => {
+    section.data.sort((pa, pb) => pa.name.localeCompare(pb.name))
+  })
+
+  return sectionList
 }
 
 export const simulatorProductList: IData[] = [
   {
     id: '1',
-    name: 'Teste 1',
-    amount: 10,
-    quantity: 1,
+    name: 'Maçã',
+    amount: 3,
+    quantity: 5,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Alimento',
     isCollected: false
   },
   {
     id: '2',
-    name: 'Teste 2',
-    amount: 20,
-    quantity: 3,
+    name: 'Banana',
+    amount: 2.5,
+    quantity: 6,
+    promotionalAmount: 2,
+    promotionalQuantity: 6,
+    section: 'Alimento',
     isCollected: true
   },
   {
     id: '3',
-    name: 'sapf oqj cbq onc oqboqne odcqk noqc npecbi bqodjcn',
-    amount: 30,
-    quantity: 1,
-    isCollected: false
+    name: 'Cândida 2L - Super Cândida',
+    amount: 4.99,
+    quantity: 2,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Limpeza',
+    isCollected: true
   },
   {
     id: '4',
-    name: 'Teste 4',
-    amount: 40,
-    quantity: 9,
+    name: 'Esponja',
+    amount: 3.49,
+    quantity: 1,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Limpeza',
     isCollected: true
   },
   {
     id: '5',
-    name: 'Teste 5',
-    amount: 50,
-    quantity: 5,
+    name: 'Café solúvel - Lór',
+    amount: 29.9,
+    quantity: 1,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Alimento',
     isCollected: false
   },
   {
     id: '6',
-    name: 'Teste 6',
-    amount: 60,
+    name: 'Absorvente',
+    amount: 13.5,
     quantity: 1,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Higiene',
     isCollected: true
   },
   {
     id: '7',
-    name: 'Teste 7',
-    amount: 70,
-    quantity: 11,
+    name: 'Papel higiênico L16P15 - Max',
+    amount: 23.07,
+    quantity: 1,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Higiene',
     isCollected: false
   },
   {
     id: '8',
-    name: 'Teste 8',
-    amount: 80,
-    quantity: 22,
+    name: 'Leite integral - Italac',
+    amount: 5.59,
+    quantity: 2,
+    promotionalAmount: 0,
+    promotionalQuantity: 0,
+    section: 'Alimento',
     isCollected: true
   },
   {
     id: '9',
-    name: 'Teste 9',
-    amount: 90,
-    quantity: 33,
-    isCollected: false
+    name: 'Sabonete - Francis',
+    amount: 3.35,
+    quantity: 8,
+    promotionalAmount: 3.19,
+    promotionalQuantity: 3,
+    section: 'Higiene',
+    isCollected: true
   },
   {
     id: '10',
-    name: 'Teste 10',
-    amount: 100,
-    quantity: 44,
-    isCollected: false
-  },
-  {
-    id: '11',
-    name: 'Teste 11',
-    amount: 110,
-    quantity: 55,
-    isCollected: true
-  },
-  {
-    id: '12',
-    name: 'Teste 12',
-    amount: 120,
-    quantity: 66,
-    isCollected: true
-  },
-  {
-    id: '13',
-    name: 'Teste 13',
-    amount: 130,
-    quantity: 77,
-    isCollected: false
-  },
-  {
-    id: '14',
-    name: 'Teste 14',
-    amount: 140,
-    quantity: 88,
-    isCollected: true
-  },
-  {
-    id: '15',
-    name: 'Teste 15',
-    amount: 150,
-    quantity: 99,
-    isCollected: false
-  },
-  {
-    id: '16',
-    name: 'Teste 16',
-    amount: 160,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '17',
-    name: 'Teste 17',
-    amount: 170,
-    quantity: 1,
-    isCollected: true
-  },
-  {
-    id: '18',
-    name: 'Teste 18',
-    amount: 180,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '19',
-    name: 'Teste 19',
-    amount: 190,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '20',
-    name: 'Teste 20',
-    amount: 200,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '21',
-    name: 'Teste 21',
-    amount: 210,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '22',
-    name: 'Teste 22',
-    amount: 220,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '23',
-    name: 'Teste 23',
-    amount: 230,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '24',
-    name: 'Teste 24',
-    amount: 240,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '25',
-    name: 'Teste 25',
-    amount: 250,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '26',
-    name: 'Teste 26',
-    amount: 260,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '27',
-    name: 'Teste 27',
-    amount: 270,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '28',
-    name: 'Teste 28',
-    amount: 280,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '29',
-    name: 'Teste 29',
-    amount: 290,
-    quantity: 1,
-    isCollected: false
-  },
-  {
-    id: '30',
-    name: 'Teste 30',
-    amount: 300,
-    quantity: 1,
+    name: 'Detergente - Minuano',
+    amount: 2.05,
+    quantity: 4,
+    promotionalAmount: 1.95,
+    promotionalQuantity: 3,
+    section: 'Limpeza',
     isCollected: false
   },
 ]
